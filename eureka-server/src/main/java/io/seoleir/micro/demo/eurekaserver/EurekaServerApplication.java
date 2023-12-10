029f159872b8466e737ac2442f38f3c6ab39b9c2
Mirolim Mirzayev <mirzayevmiralim28@gmail.com>
2023-12-11 04:28:07 +0500

    Add eureka-client

--- eureka-server/src/main/java/io/seoleir/micro/demo/eurekaserver/EurekaServerApplication.java
+++ eureka-server/src/main/java/io/seoleir/micro/demo/eurekaserver/EurekaServerApplication.java
@@ -2,8 +2,9 @@ package io.seoleir.micro.demo.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-
+@EnableEurekaServer
 @SpringBootApplication
 public class EurekaServerApplication {
 
