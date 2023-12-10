9eb046cd021627f818f5b0c12764c75e998294bb
Mirolim Mirzayev <mirzayevmiralim28@gmail.com>
2023-12-10 17:53:29 +0500

    Configure build.gradle file in eureka server and base project

--- eureka-server/src/main/java/io/seoleir/micro/demo/eurekaserver/EurekaServerApplication.java
+++ eureka-server/src/main/java/io/seoleir/micro/demo/eurekaserver/EurekaServerApplication.java
@@ -3,6 +3,7 @@ package io.seoleir.micro.demo.eurekaserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class EurekaServerApplication {
 
