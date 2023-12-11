956452df51a7d89c4476eb79c349e959a354918c
Mirolim Mirzayev <mirzayevmiralim28@gmail.com>
2023-12-11 10:47:27 +0500

    Add eureka client 2

---
+++ eureka-client-2/src/main/java/io/seoleir/micro/demo/eurekaclient2/EurekaClient2Application.java
@@ -0,0 +1,15 @@
+package io.seoleir.micro.demo.eurekaclient2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class EurekaClient2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaClient2Application.class, args);
+    }
+
+}
---
+++ eureka-client-2/src/main/java/io/seoleir/micro/demo/eurekaclient2/controller/NewController.java
@@ -0,0 +1,15 @@
+package io.seoleir.micro.demo.eurekaclient2.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/new")
+public class NewController {
+
+    @GetMapping("/name")
+    public String getName(){
+        return "Hello world";
+    }
+}
