9c3954e66b9ba138045036c4aeaed21254f79a0b
Mirolim Mirzayev <mirzayevmiralim28@gmail.com>
2023-12-11 17:48:39 +0500

    Implement spring cloud config to eureka-client

--- eureka-client/src/main/java/io/seoleir/micro/demo/eurekaclient/controller/TestController.java
+++ eureka-client/src/main/java/io/seoleir/micro/demo/eurekaclient/controller/TestController.java
@@ -1,6 +1,7 @@
 package io.seoleir.micro.demo.eurekaclient.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,7 +14,7 @@ public class TestController {
     private String instanceId;
 
     @GetMapping("/test")
-    public String test(){
-        return instanceId;
+    public ResponseEntity<String> test(){
+        return ResponseEntity.ok(instanceId);
     }
 }
