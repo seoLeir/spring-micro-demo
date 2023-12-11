10614455f7b72377501b3ec6b2d3e13c09730a8b
Mirolim Mirzayev <mirzayevmiralim28@gmail.com>
2023-12-11 12:53:19 +0500

    Add github cloud config server

---
+++ github-cloud-config-server/src/main/java/io/seoleir/micro/demo/githubcloudconfigserver/GithubCloudConfigServerApplication.java
@@ -0,0 +1,13 @@
+package io.seoleir.micro.demo.githubcloudconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubCloudConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubCloudConfigServerApplication.class, args);
+    }
+
+}
---
+++ github-cloud-config-server/src/test/java/io/seoleir/micro/demo/githubcloudconfigserver/GithubCloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.seoleir.micro.demo.githubcloudconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubCloudConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
